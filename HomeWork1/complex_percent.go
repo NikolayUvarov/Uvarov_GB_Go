6c6e5fe7cd26e9947939d038b9de5187daab2c27
un-work@i7 <un-work@i7>
2020-04-19 22:15:51 +0300

    hw1

--- HomeWork1/complex_percent.go
+++ HomeWork1/complex_percent.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+	"fmt"
+	"math"
+)
+
+var rub float64 = 0
+var percent float64 = 0
+
+const years int = 5
+
+func main() {
+	fmt.Print("Cложный процент за ", years, " лет.\nВведите сумму вклада и проценты за год через пробел:")
+	n, err := fmt.Scanln(&rub, &percent)
+	if err != nil || n != 2 {
+		fmt.Errorf("Error")
+	}
+
+	fmt.Printf("Cумма за %d лет: %.2f", years, rub*math.Pow((1+percent/100), float64(years)))
+
+}
